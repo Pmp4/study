37adcaa2590f2da9e634a0bea04ea37af34441b2
P__.mp4 <postlist.ed@gmail.com>
2023-11-05 20:53:34 +0900

    20231105

--- sb+jpa_kyh/part_1/jpashop/src/main/java/jpabook/jpashop/domain/Item/Item.java
+++ sb+jpa_kyh/part_1/jpashop/src/main/java/jpabook/jpashop/domain/Item/Item.java
@@ -1,8 +1,8 @@
-package jpabook.jpashop.domain.item;
+package jpabook.jpashop.domain;
 
-import jpabook.jpashop.domain.Category;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
@@ -11,21 +11,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "dtype")
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
-public abstract class Item {
+public class Item {
     @Id
     @GeneratedValue
     @Column(name = "item_id")
     private Long id;
 
     private String name;
+
     private int price;
+
     private int stockQuantity;
 
-    @ManyToMany(mappedBy = "items")
+    @ManyToMany
     private List<Category> categories = new ArrayList<>();
 }
